4656ebf7e40349219caa9116bb86afbe453cdad0
Felix Honer <felix.honer@novatec-gmbh.de>
2021-01-16 12:39:51 +0100

    Configure JWT expiry via application.yml

--- src/main/kotlin/com/felixhoner/schichtplaner/api/security/JwtSigner.kt
+++ src/main/kotlin/com/felixhoner/schichtplaner/api/security/JwtSigner.kt
@@ -2,6 +2,7 @@ package com.felixhoner.schichtplaner.api.security
 
 import io.jsonwebtoken.*
 import io.jsonwebtoken.security.Keys
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import java.time.Duration
 import java.time.Instant
@@ -10,15 +11,21 @@ import java.util.*
 @Service
 class JwtSigner {
 
-	private val key =
-		Keys.hmacShaKeyFor("3ruDg8e2OPHWIJrMEdwahJFFBmVLwRdxis7TFPkml-Sb2Hqr9HbhKcEpBSHS_91ohipPeCuOGvCU97FJFLvD3RsRPyqN5eN2hehT87XnRRYSSMG91hd3P_5h4jPGijBfAqW_66tEFqtMG6hmK6EzgkZdT1Dy1fpddxl-aTw2KZt3PH_nJ1K9jCjkNIRCOSHv5Et8olr-jDDoi9MSqE1hg-zXWFmETWWv-aIDxfRcWVuj_i1jCrfpSCBcStRhI-rao3j6lBppYRrE9MkGzNCAbyiflxK6iwYek63yxAky2stlYj5oKuIh-4tLASWOyBHJY4vjr5wZ2fCJ2KePrUMNnw".toByteArray())
+	@Value("\${authorization.key}")
+	lateinit var key: String
+
+	@Value("\${authorization.expireAfterMinutes.accessToken}")
+	var accessTokenExpiredAfter: Long = 0
+
+	@Value("\${authorization.expireAfterMinutes.refreshToken}")
+	var refreshTokenExpiredAfter: Long = 0
 
 	fun createAccessToken(userId: String, roles: List<String>): String {
 		return Jwts.builder()
-			.signWith(key)
+			.signWith(Keys.hmacShaKeyFor(key.toByteArray()))
 			.setSubject(userId)
 			.setIssuer("identity")
-			.setExpiration(Date.from(Instant.now().plus(Duration.ofMinutes(15))))
+			.setExpiration(Date.from(Instant.now().plus(Duration.ofMinutes(accessTokenExpiredAfter))))
 			.setIssuedAt(Date.from(Instant.now()))
 			.claim("roles", roles)
 			.claim("type", "access")
@@ -27,10 +34,10 @@ class JwtSigner {
 
 	fun createRefreshToken(userId: String, roles: List<String>): String {
 		return Jwts.builder()
-			.signWith(key)
+			.signWith(Keys.hmacShaKeyFor(key.toByteArray()))
 			.setSubject(userId)
 			.setIssuer("identity")
-			.setExpiration(Date.from(Instant.now().plus(Duration.ofMinutes(1440))))
+			.setExpiration(Date.from(Instant.now().plus(Duration.ofMinutes(refreshTokenExpiredAfter))))
 			.setIssuedAt(Date.from(Instant.now()))
 			.claim("roles", roles)
 			.claim("type", "refresh")
