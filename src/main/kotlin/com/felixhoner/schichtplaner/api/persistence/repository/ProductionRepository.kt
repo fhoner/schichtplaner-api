735dd662bcaa9ac3516fc38f65f5748e8e12fcf6
Felix Honer <felix.honer@novatec-gmbh.de>
2021-02-28 22:41:34 +0100

    wip

--- src/main/kotlin/com/felixhoner/schichtplaner/api/persistence/repository/ProductionRepository.kt
+++ src/main/kotlin/com/felixhoner/schichtplaner/api/persistence/repository/ProductionRepository.kt
@@ -4,11 +4,14 @@ import com.felixhoner.schichtplaner.api.persistence.entity.ProductionEntity
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
+import java.util.*
 
 @Repository
 interface ProductionRepository : CrudRepository<ProductionEntity, Long> {
 
     @Query("select p from ProductionEntity p where p.plan.id in :planIds")
     fun findAllByPlanIds(planIds: List<Long>): List<ProductionEntity>
+
+    fun findByUuid(planUuid: UUID): ProductionEntity?
 
 }
