2bbe6b09d59e351614d1dc930cbad7af2cb8013d
Felix Honer <felix.honer@novatec-gmbh.de>
2021-03-28 22:05:24 +0200

    Fixed build

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -2,9 +2,6 @@ plugins {
     `kotlin-dsl`
 }
 
-val wrapperUser: String = System.getProperty("gradle.wrapperUser")
-val wrapperPassword: String = System.getProperty("gradle.wrapperPassword")
-
 repositories {
     mavenCentral()
     google()
